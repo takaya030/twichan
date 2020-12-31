71b41740d9152ef9ab0c6afb81455d42ac3fc2f9
Minoru Takayama <takaya030@yahoo.co.jp>
2020-12-31 13:03:13 +0900

    add timeline view

--- app/Http/Controllers/TwitterController.php
+++ app/Http/Controllers/TwitterController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers;
 use \Illuminate\Http\Request;
 
 use App\Models\Twitter\Timeline;
+use App\Models\Twitter\Tweet;
 
 class TwitterController extends Controller
 {
@@ -23,7 +24,17 @@ class TwitterController extends Controller
 	{
 		$model = new Timeline();
 		$result = $model->getTimeline();
+		//dd($result);
 
-		dd($result);
+		$tweets = [];
+		if( is_array($result) )
+		{
+			foreach( $result as $tw )
+			{
+				$tweets[] = new Tweet( $tw );
+			}
+		}
+
+		return view("timeline", ['tweets' => $tweets]);
 	}
 }

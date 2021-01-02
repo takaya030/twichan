8b99e0bf9121bbc97e73f7b6ff29b2c6513b266f
Minoru Takayama <takaya030@yahoo.co.jp>
2021-01-02 17:35:24 +0900

    modify timeline template

--- resources/views/timeline.blade.php
+++ resources/views/timeline.blade.php
@@ -3,8 +3,8 @@
 @section('title', 'Timeline')
 
 @section('content')
-	<script async src="https://platform.twitter.com/widgets.js" charset="utf-8"></script>
 	@foreach( $tweets as $tweet )
-		<blockquote class="twitter-tweet"><a href="{{ $tweet->getStatusUrl() }}?ref_src=twsrc%5Etfw"></a></blockquote>
+		<blockquote class="twitter-tweet"><p lang="{{ $tweet->getLang() }}" dir="ltr">{{ $tweet->getFullText() }}</p>&mdash; {{ $tweet->getName() }} @({{ $tweet->getScreenName() }}) <a href="{{ $tweet->getStatusUrl() }}?ref_src=twsrc%5Etfw">{{ $tweet->getCreatedAt() }}</a></blockquote>
 	@endforeach
+	<script async src="https://platform.twitter.com/widgets.js" charset="utf-8"></script>
 @endsection
